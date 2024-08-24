8ceb3f9284970eaf803dea0e03133e4bae5bc44b
froilan <61849282+froilanimnida@users.noreply.github.com>
2024-08-24 15:45:25 +0800

    fix 404 not found on deployment during build and output of the build the vercel renders 404 error and this will remediate that error. (#8)
    * changes the build and distributable path to prevent 404

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# OnlineVotingSystem
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.0.
 
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'online-voting-system' title`, () => {
+  it(`should have the 'frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('online-voting-system');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, online-voting-system');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
 });
