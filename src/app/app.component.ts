5521fc83ccb22c4ee4d7f66644c11083a2595f4d
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-08-26 13:55:15 +0800

    refactor for cleaner commit history as per recommendation

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, ButtonModule],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+})
+export class AppComponent {
+	title = 'frontend';
+}
