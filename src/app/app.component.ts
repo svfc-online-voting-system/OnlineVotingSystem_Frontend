9407c55d580509dc3267aed95a6cea0a21e8799b
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-08-26 17:50:19 +0800

    added coming soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
-	templateUrl: './app.component.html',
-	styleUrl: './app.component.css',
+	imports: [RouterLink],
+	template: `
+		<main
+			class="main w-full min-h-screen flex flex-col justify-center items-center bg-gray-100 gap-20"
+		>
+			<div>
+				<h1
+					class="font-bold text-lg md:text-xl lg:text-3xl xl:text-7xl"
+				>
+					Coming soon...
+				</h1>
+			</div>
+			<a routerLink="features" class="bg-pink-200 w-fit rounded-lg p-4">
+				Features
+			</a>
+		</main>
+	`,
+	styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-	title = 'Coming Soon';
-	imageUrl = '/assets/images/root/bg-coming-soon.jpg';
-}
+export class AppComponent {}
