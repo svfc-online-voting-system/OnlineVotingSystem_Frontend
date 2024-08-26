a2729e408e93661fb4acc67edb28e7262c546c9c
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-08-26 15:58:24 +0800

    changes on configuration and cleared boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'frontend' title`, () => {
+  it(`should have the 'online-voting-system' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('online-voting-system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, online-voting-system');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, ButtonModule],
+	imports: [RouterOutlet],
 	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-	title = 'frontend';
+	title = 'Coming Soon';
+	imageUrl = '/assets/images/root/bg-coming-soon.jpg';
 }
