cf2540e91f2274336d6d653bb02b19dcd2ba9c4d
froilan <61849282+froilanimnida@users.noreply.github.com>
2024-08-29 22:52:03 +0800

    added forms for login and signup (#18)
    * fix inline font issue
    
    * initialize some auth pages
    
    * added boilerplate for auth forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { FeaturesComponent } from './features/features.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent, title: 'Coming Soon' },
 	{ path: 'features', component: FeaturesComponent, title: 'Features' },
+	{ path: 'login', component: LoginComponent, title: 'Login' },
+	{ path: 'signup', component: SignupComponent, title: 'Signup' },
 ];
