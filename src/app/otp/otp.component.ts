f0d06a8a08d0a9cd1b0c67e9b51836d213d4a773
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-08-30 22:19:33 +0800

    Added business benefits

---
+++ src/app/otp/otp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otp',
+  standalone: true,
+  imports: [],
+  templateUrl: './otp.component.html',
+  styleUrl: './otp.component.css'
+})
+export class OtpComponent {
+
+}
