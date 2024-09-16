45fd67ba289bca6a94605524412f1fe2e17e0cad
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-09-16 17:10:23 +0800

    added admin routes boilerplate

--- src/app/shared/ui/loading-dialog/dialog.component.spec.ts
+++ src/app/shared/ui/loading-dialog/dialog.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog'
 import { DialogComponent } from './dialog.component';
 
 describe('DialogComponent', () => {
 	let component: DialogComponent;
 	let fixture: ComponentFixture<DialogComponent>;
+	let mockDialogRef: jasmine.SpyObj<MatDialogRef<DialogComponent>>;
 
 	beforeEach(async () => {
+		// Create a mock instance of MatDialogRef
+		mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
 		await TestBed.configureTestingModule({
-			imports: [DialogComponent, MatDialogRef],
+			imports: [MatDialogModule],
+			declarations: [DialogComponent],
+			providers: [{ provide: MatDialogRef, useValue: mockDialogRef }],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(DialogComponent);
