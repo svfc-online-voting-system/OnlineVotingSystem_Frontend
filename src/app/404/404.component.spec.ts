22a61ad018e6fe872f011d14d1f78bd03413d064
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-09-17 11:32:58 +0800

    added disabled button on submit on login

--- src/app/404/404.component.spec.ts
+++ src/app/404/404.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { NotFoundComponent } from './404.component';
+import { of } from 'rxjs';
 
 describe('NotFoundComponent', () => {
 	let component: NotFoundComponent;
@@ -9,6 +10,15 @@ describe('NotFoundComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [NotFoundComponent],
+			providers: [
+				{
+					provide: ActivatedRoute,
+					useValue: {
+						snapshot: { paramMap: { get: () => 'mockId' } },
+						params: of({ id: 'mockId' }),
+					},
+				},
+			],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(NotFoundComponent);
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,12 @@
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '@app/services/api/auth/auth.service';
-import { Component, ViewChild, OnInit, inject } from '@angular/core';
+import {
+	Component,
+	ViewChild,
+	OnInit,
+	inject,
+	afterNextRender,
+} from '@angular/core';
 import {
 	FormBuilder,
 	Validators,
@@ -44,6 +50,7 @@ import { SnackbarService } from '@app/services/snackbar/snackbar.service';
 })
 export class LoginComponent implements OnInit {
 	@ViewChild('showPasswordToggler') showPasswordToggler!: MatCheckbox;
+	@ViewChild('loginButton') loginButton!: HTMLButtonElement;
 	readonly dialog = inject(MatDialog);
 
 	emailFormGroup!: FormGroup;
@@ -54,7 +61,13 @@ export class LoginComponent implements OnInit {
 		private _authService: AuthService,
 		private _logInValidatorService: LoginValidatorsService,
 		private _router: Router
-	) {}
+	) {
+		afterNextRender(() => {
+			this.togglePasswordVisibility();
+			this.disableLoginButtonOnSubmit();
+			this.enableLoginButton();
+		});
+	}
 
 	ngOnInit(): void {
 		this.emailFormGroup = this._formBuilder.group({
@@ -93,6 +106,16 @@ export class LoginComponent implements OnInit {
 		}
 	}
 
+	disableLoginButtonOnSubmit(): void {
+		this.loginButton.textContent = 'Logging in...';
+		this.loginButton.disabled = true;
+	}
+
+	enableLoginButton(): void {
+		this.loginButton.textContent = 'Log in';
+		this.loginButton.disabled = false;
+	}
+
 	submitEmailForm(): void {
 		const emailValidationResult = this._logInValidatorService.validateEmail(
 			this.emailFormGroup
@@ -119,9 +142,11 @@ export class LoginComponent implements OnInit {
 		if (this.emailFormGroup.valid && this.passwordFormGroup.valid) {
 			const emailFormGroupValue = this.emailFormGroup.value;
 			const passwordFormgroupValue = this.passwordFormGroup.value;
-
-			try {
+			const dialogTimer = setTimeout(() => {
 				this.openLoadingDialog();
+			}, 2000);
+			this.disableLoginButtonOnSubmit();
+			try {
 				const response: ApiAuthResponse = await this._authService.login(
 					{
 						email: emailFormGroupValue.email,
@@ -140,7 +165,9 @@ export class LoginComponent implements OnInit {
 				errorMessage = errorObj.error.message;
 				this._snackBarService.showSnackBar(errorMessage);
 			} finally {
+				clearTimeout(dialogTimer);
 				this.closeLoadingDialog();
+				this.enableLoginButton();
 			}
 		} else {
 			this._snackBarService.showSnackBar(
