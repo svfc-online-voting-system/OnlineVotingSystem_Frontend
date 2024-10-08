cf3d5db3f017ca99935f88e17134b577f650786a
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-10-08 18:33:33 +0800

    added some enhancement in the loading screen

--- src/app/auth/verify-email/verify-email.component.spec.ts
+++ src/app/auth/verify-email/verify-email.component.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { VerifyEmailComponent } from './verify-email.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('VerifyEmailComponent', () => {
-  let component: VerifyEmailComponent;
-  let fixture: ComponentFixture<VerifyEmailComponent>;
+	let component: VerifyEmailComponent;
+	let fixture: ComponentFixture<VerifyEmailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [VerifyEmailComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [VerifyEmailComponent],
+			providers: [
+				{
+					provide: ActivatedRoute,
+					useValue: {
+						snapshot: { paramMap: { get: () => 'mockId' } },
+						params: of({ id: 'mockId' }),
+					},
+				},
+			],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(VerifyEmailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(VerifyEmailComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
