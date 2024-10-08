19eae5df0f52bbb8c395b9363a322eca775beb97
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-10-08 18:37:19 +0800

    fix tests error

--- src/app/auth/verify-email/verify-email.component.spec.ts
+++ src/app/auth/verify-email/verify-email.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { VerifyEmailComponent } from './verify-email.component';
 import { ActivatedRoute } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('VerifyEmailComponent', () => {
 	let component: VerifyEmailComponent;
@@ -9,8 +11,9 @@ describe('VerifyEmailComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [VerifyEmailComponent],
+			imports: [VerifyEmailComponent, BrowserAnimationsModule],
 			providers: [
+				provideHttpClient(),
 				{
 					provide: ActivatedRoute,
 					useValue: {
