57f8a63685c9d535a2b9556ecd3aa120951aa7c2
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-10-12 20:11:23 +0800

    fix test case errors

--- src/app/authenticated/admin/manage-admins/manage-admins.component.spec.ts
+++ src/app/authenticated/admin/manage-admins/manage-admins.component.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { ManageAdminsComponent } from './manage-admins.component';
 
 describe('ManageAdminsComponent', () => {
-  let component: ManageAdminsComponent;
-  let fixture: ComponentFixture<ManageAdminsComponent>;
+	let component: ManageAdminsComponent;
+	let fixture: ComponentFixture<ManageAdminsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ManageAdminsComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ManageAdminsComponent],
+			providers: [
+				{
+					provide: ActivatedRoute,
+					useValue: {
+						snapshot: { paramMap: { get: () => 'mockId' } },
+						params: of({ id: 'mockId' }),
+					},
+				},
+			],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(ManageAdminsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(ManageAdminsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/authenticated/admin/manage-elections/manage-elections.component.spec.ts
+++ src/app/authenticated/admin/manage-elections/manage-elections.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { ManageElectionsComponent } from './manage-elections.component';
 
 describe('ManageElectionsComponent', () => {
-  let component: ManageElectionsComponent;
-  let fixture: ComponentFixture<ManageElectionsComponent>;
+	let component: ManageElectionsComponent;
+	let fixture: ComponentFixture<ManageElectionsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ManageElectionsComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ManageElectionsComponent],
+			providers: [
+				{
+					providers: [
+						{
+							provide: ActivatedRoute,
+							useValue: {
+								snapshot: { paramMap: { get: () => 'mockId' } },
+								params: of({ id: 'mockId' }),
+							},
+						},
+					],
+				},
+			],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(ManageElectionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(ManageElectionsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
