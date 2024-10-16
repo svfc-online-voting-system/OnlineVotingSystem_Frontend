d50beee455abecc159572dfaa0844d8cb8211aee
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-10-16 23:47:08 +0800

    added poll options low level logic

--- src/app/shared/ui/new-poll/new-poll.component.ts
+++ src/app/shared/ui/new-poll/new-poll.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '@app/shared/ui/user/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
 	selector: 'app-new-poll',
 	standalone: true,
-	imports: [NavbarComponent],
+	imports: [
+		NavbarComponent,
+		ReactiveFormsModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatIcon,
+		MatButtonModule,
+	],
 	templateUrl: './new-poll.component.html',
 })
-export class NewPollComponent {}
+export class NewPollComponent {
+	pollOptions: string[] = [];
+	addOption() {
+		this.pollOptions.push('');
+	}
+
+	saveUpdatedPoll() {
+		this.pollOptions = this.pollOptions.filter((option) => option !== '');
+	}
+}
