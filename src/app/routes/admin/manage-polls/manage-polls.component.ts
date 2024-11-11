39baf5248e3782a3bca87c68dfa937ab4cd5f791
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-11-11 18:07:47 +0800

    feat: add Manage Polls component and update navbar for navigation

---
+++ src/app/routes/admin/manage-polls/manage-polls.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-polls',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-polls.component.html',
+  styleUrl: './manage-polls.component.scss'
+})
+export class ManagePollsComponent {
+
+}
--- src/app/shared/ui/admin/navbar/navbar.component.ts
+++ src/app/shared/ui/admin/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService, SnackbarService } from '@app/core/core.module';
+import { DialogService } from '@app/core/services/dialog/dialog.service';
 
 @Component({
 	selector: 'app-admin-navbar',
@@ -23,17 +24,37 @@ export class NavbarComponent {
 	private readonly _snackBarService = inject(SnackbarService);
 	private readonly _authService = inject(AuthService);
 	private readonly _router = inject(Router);
+	private readonly _dialogBox = inject(DialogService);
 
-	logoutCurrentSession() {
-		this._authService.logoutSession().subscribe({
-			next: () => {
-				this._snackBarService.showSnackBar('Logged out successfully');
-				this._router.navigateByUrl('/');
-			},
-			error: (error) => {
-				this._snackBarService.showSnackBar('Failed to log out');
-				console.error('Error logging out:', error);
-			},
-		});
+	async logoutCurrentSession() {
+		this._dialogBox
+			.openConfirmationDialog({
+				title: 'Logout',
+				message: 'Are you sure you want to log out?',
+				confirmText: 'Yes',
+				cancelText: 'No',
+				showCancel: true,
+				isHTML: false,
+			})
+			.subscribe({
+				next: (confirmed) => {
+					if (confirmed) {
+						this._authService.logoutSession().subscribe({
+							next: () => {
+								this._snackBarService.showSnackBar(
+									'Logged out successfully',
+								);
+								this._router.navigateByUrl('/');
+							},
+							error: (error) => {
+								this._snackBarService.showSnackBar(
+									'Failed to log out',
+								);
+								console.error('Error logging out:', error);
+							},
+						});
+					}
+				},
+			});
 	}
 }
