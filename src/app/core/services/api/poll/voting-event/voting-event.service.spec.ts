0d3464bcc0c740b087190c3f672dda33e8464627
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-11-13 22:38:07 +0800

    fix test bed error

--- src/app/core/services/api/poll/voting-event/voting-event.service.spec.ts
+++ src/app/core/services/api/poll/voting-event/voting-event.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { VotingEventService } from './voting-event.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('VotingEventService', () => {
 	let service: VotingEventService;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			providers: [provideHttpClient()],
+		});
 		service = TestBed.inject(VotingEventService);
 	});
 
--- src/app/core/services/api/profile/profile.service.spec.ts
+++ src/app/core/services/api/profile/profile.service.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ProfileService } from './profile.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ProfileService', () => {
-  let service: ProfileService;
+	let service: ProfileService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProfileService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [provideHttpClient()],
+		});
+		service = TestBed.inject(ProfileService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
